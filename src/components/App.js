29379c06397c2772619d9c9f36eebf75f541851e
Nihal00 <nihallsuvarna@gmail.com>
2023-07-11 00:12:40 +0530

    submitting code-Tue Jul 11 2023 00:12:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [data, setData] = useState(0);
+  function click(){
+    setData(data+1);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {data} times</p>
+        <button onClick={click}>Click me</button>
     </div>
   )
 }
